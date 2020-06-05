109476038ae7bf1f1bbf620640480f99280ab58a
GersonSalvador <zecoevane@yahoo.com.br>
2020-06-05 18:28:53 -0300

    Layout da página Criar pontos de coleta

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi'
 import './styles.css';
 
 import logo from '../../assets/logo.svg';
@@ -9,7 +10,104 @@ const CreatPoint = () => {
     <div id="page-create-point">
       <header>
         <img src={logo} alt="Ecoleta"/>
+        <Link to="/">
+          <FiArrowLeft />
+          Voltar para Home
+        </Link>
       </header>
+
+      <form>
+        <h1>Cadastro de ponto de coleta</h1>
+        
+        <fieldset>
+          <legend>
+            <h2>Dados</h2>
+          </legend>
+
+            <div className="field">
+              <label htmlFor="name">Nome</label>
+              <input 
+                type="text"
+                name="name"
+                id="name"
+              />
+            </div>
+
+            <div className="field-group">
+              <div className="field">
+                <label htmlFor="email">E-mail</label>
+                <input 
+                  type="email"
+                  name="email"
+                  id="email"
+                />
+              </div>
+              <div className="field">
+                <label htmlFor="whatsapp">Whatsapp</label>
+                <input 
+                  type="text"
+                  name="whatsapp"
+                  id="whatsapp"
+                />
+              </div>
+            </div>
+        </fieldset>
+        <fieldset>
+          <legend>
+            <h2>Endereço</h2>
+            <span>Selecione o endereço no mapa</span>
+          </legend>
+
+          <div className="field-group">
+              <div className="field">
+                <label htmlFor="email">Estado (UF)</label>
+                <select name="uf" id="uf">
+                  <option value="">-- Selecione --</option>
+                </select>
+              </div>
+              <div className="field">
+                <label htmlFor="whatsapp">Cidade</label>
+                <select name="city" id="city">
+                  <option value="">-- Selecione --</option>
+                </select>
+              </div>
+            </div>
+
+        </fieldset>
+        <fieldset>
+          <legend>
+            <h2>Ítens de coleta</h2>
+            <span>Selecione um ou mais ítens abaxo</span>
+          </legend>
+          <ul className="items-grid">
+            <li>
+              <img src="http://localhost:3333/uploads/lampadas.svg" alt="Lâmpadas"/>
+              <span>Lâmpadas</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/lampadas.svg" alt="Lâmpadas"/>
+              <span>Lâmpadas</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/lampadas.svg" alt="Lâmpadas"/>
+              <span>Lâmpadas</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/lampadas.svg" alt="Lâmpadas"/>
+              <span>Lâmpadas</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/lampadas.svg" alt="Lâmpadas"/>
+              <span>Lâmpadas</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/lampadas.svg" alt="Lâmpadas"/>
+              <span>Lâmpadas</span>
+            </li>
+          </ul>
+        </fieldset>
+        <button type="submit">Cadastrar ponto de coleta</button>
+      </form>
     </div>
   )
 }
