523c753451f2bed17273bc1840006516d885faab
GersonSalvador <zecoevane@yahoo.com.br>
2020-06-05 18:46:45 -0300

    Added map on Create Point Page

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi'
+import { Map, TileLayer, Marker } from 'react-leaflet'
 import './styles.css';
 
 import logo from '../../assets/logo.svg';
@@ -57,6 +58,14 @@ const CreatPoint = () => {
             <h2>Endereço</h2>
             <span>Selecione o endereço no mapa</span>
           </legend>
+          
+          <Map center={[-26.892553,-49.2334694]} zoom={15}>
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[-26.892553,-49.2334694]} />
+          </Map>
 
           <div className="field-group">
               <div className="field">
